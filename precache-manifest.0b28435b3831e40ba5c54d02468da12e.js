16c13925a20db528a46fcdff0f8e8085a606214d
MeidanLeidemran <meidan.leiderman@gmail.com>
2020-01-13 18:33:18 +0200

    Updates

---
+++ precache-manifest.0b28435b3831e40ba5c54d02468da12e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c311e0c4e644c5fb69b7c4a373a7bf3e",
+    "url": "/mister-bitcoin/index.html"
+  },
+  {
+    "revision": "e27c233f5e02c533d870",
+    "url": "/mister-bitcoin/static/css/main.974e1775.chunk.css"
+  },
+  {
+    "revision": "724f51f68ce72d6dfeaf",
+    "url": "/mister-bitcoin/static/js/2.aee16db3.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/mister-bitcoin/static/js/2.aee16db3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e27c233f5e02c533d870",
+    "url": "/mister-bitcoin/static/js/main.369bb04d.chunk.js"
+  },
+  {
+    "revision": "c2900914f2c7369dd81c",
+    "url": "/mister-bitcoin/static/js/runtime-main.9befb8b2.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/mister-bitcoin/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
